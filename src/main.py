13e1fe9b832a42751bd39b79cc25de4812989d2f
Nguyen Dang Quang <nguyendangquang@student-10-201-47-098.wireless.adelaide.edu.au>
2022-08-18 16:27:11 +0930

    2nd version

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ from torchsummary import summary
 import matplotlib.pyplot as plt
 from PIL import Image
 from utils import trigger_detector, show, backdoor_infer, add_white_trigger, add_checkerboard_trigger
+from train_alex_cifar import AlexNet
 
 
 transform = transforms.Compose([
@@ -33,49 +34,6 @@ trainset = torchvision.datasets.CIFAR10(root='~/data',
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse',
            'ship', 'truck')
-
-
-class AlexNet(nn.Module):
-
-    def __init__(self, num_classes):
-        super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(0.5),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
-        logits = self.classifier(x)
-        probas = F.softmax(logits, dim=1)
-        return logits
 
 
 model = AlexNet(10)
